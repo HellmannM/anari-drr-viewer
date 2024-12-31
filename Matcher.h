fadaee4ec34f36ee8adef92b4235cfd97a86cd8d
Matthias Hellmann <m.hellmann@me.com>
2024-12-31 01:25:05 +0100

    extend matcher interface with update_camera and calibrate functions

--- Matcher.h
+++ Matcher.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <dlfcn.h>
 #include <cstdint>
 #include <string>
@@ -9,12 +10,29 @@ struct feature_matcher
 {
   enum PIXEL_TYPE
   {
-    UNKNOWN = 0,
+    PIXEL_TYPE_UNKNOWN = 0,
     RGBA = 1
   };
 
-  virtual void set_reference_image(const std::vector<uint8_t> &data, int width, int height, PIXEL_TYPE pixel_type);
-  virtual void match(const std::vector<uint8_t> &data, int width, int height, PIXEL_TYPE pixel_type);
+  enum DEPTH_TYPE
+  {
+    DEPTH_TYPE_UNKNOWN = 0,
+    FLOAT = 1,
+    FLOAT3 = 2
+  };
+
+  virtual void set_reference_image(const std::vector<uint8_t> &data, size_t width, size_t height, PIXEL_TYPE pixel_type);
+  virtual void match(const std::vector<uint8_t> &data, size_t width, size_t height, PIXEL_TYPE pixel_type);
+  virtual void calibrate(size_t width, size_t height, float fovy, float aspect);
+  virtual bool update_camera(const std::vector<float>& depth,
+                             const std::vector<float>& depth3D,
+                             size_t width,
+                             size_t height,
+                             DEPTH_TYPE depth_type,
+                             DEPTH_TYPE depth3D_type,
+                             std::array<float, 3>& eye,
+                             std::array<float, 3>& center,
+                             std::array<float, 3>& up);
 };
 
 struct MatchersWrapper
