cd56e9792b960660c2e38669362330df83cd087e
Matthias Hellmann <m.hellmann@me.com>
2025-01-14 22:41:13 +0100

    set texture wrapping mode to clamp_to_edge

--- ImageViewport.cpp
+++ ImageViewport.cpp
@@ -17,6 +17,8 @@ ImageViewport::ImageViewport(const std::vector<Image>& images, const char *name)
   glBindTexture(GL_TEXTURE_2D, m_framebufferTexture);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexImage2D(GL_TEXTURE_2D,
       0,
       GL_RGBA8,
--- Viewport.cpp
+++ Viewport.cpp
@@ -32,6 +32,8 @@ DRRViewport::DRRViewport(anari::Device device, visionaray::pinhole_camera &camer
   glBindTexture(GL_TEXTURE_2D, m_framebufferTexture);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glTexImage2D(GL_TEXTURE_2D,
       0,
       GL_RGBA8,
